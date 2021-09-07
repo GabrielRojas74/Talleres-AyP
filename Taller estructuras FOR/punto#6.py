5d5042350407c822f0757bca3a02d4ef7b0fbc8e
Gabriel Santiago Rojas Leon <grojasl21353@universidadean.edu.co>
2021-09-07 08:44:52 -0500

    taller

---
+++ Taller estructuras FOR/punto#6.py
@@ -0,0 +1,17 @@
+"""6. Cuente e Imprima las ciudades que su pais inicie con la letra E"""
+archivo = open('paises.txt', 'r')
+lista = []
+paises = []
+for i in archivo:
+  a = i.index(":")
+  for r in range(0, a):
+    lista.append(i[r])
+  a = "".join(lista)
+  paises.append(a)
+  lista = []
+for i in paises:
+  if(i[0] == "E"):
+    print(i)
+    lista.append(i)
+print(len(lista))
+archivo.close()
